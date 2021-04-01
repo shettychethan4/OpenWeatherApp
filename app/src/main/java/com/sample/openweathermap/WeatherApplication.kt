c2a6d066994858277ff7afd6733dc54a03c09785
Chethan Shetty <chethan.shetty@rytle.com>
2021-04-02 00:58:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/sample/openweathermap/WeatherApplication.kt
@@ -0,0 +1,7 @@
+package com.sample.openweathermap
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WeatherApplication : Application()
---
+++ app/src/main/java/com/sample/openweathermap/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sample.openweathermap.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.sample.openweathermap.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
